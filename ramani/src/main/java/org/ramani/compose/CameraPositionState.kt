595ed114bdb0d727207c228bdef6fc8b3eba1f6c
Jonas Vautherin <jonas.vautherin@gmail.com>
2023-06-20 14:16:20 +0200

    Add copyright to CameraPositionState

--- ramani/src/main/java/org/ramani/compose/CameraPositionState.kt
+++ ramani/src/main/java/org/ramani/compose/CameraPositionState.kt
@@ -1,3 +1,13 @@
+/*
+ * This file is part of ramani-maps.
+ *
+ * Copyright (c) 2023 Roman Bapst & Jonas Vautherin.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package org.ramani.compose
 
 import android.os.Parcel

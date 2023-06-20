a6979eeac0eb84c1d6e140ff831e69a12e99ed93
Jonas Vautherin <jonas.vautherin@gmail.com>
2023-06-20 13:36:04 +0200

    CameraPosition needs to be parcelable

--- ramani/src/main/java/org/ramani/compose/CameraPositionState.kt
+++ ramani/src/main/java/org/ramani/compose/CameraPositionState.kt
@@ -1,5 +1,7 @@
 package org.ramani.compose
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -10,13 +12,41 @@ data class CameraPosition(
     var zoom: Double? = null,
     var tilt: Double? = null,
     var bearing: Double? = null,
-) {
+) : Parcelable {
     constructor(cameraPosition: CameraPosition) : this(
         cameraPosition.target,
         cameraPosition.zoom,
         cameraPosition.tilt,
         cameraPosition.bearing
     )
+
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(LatLng::class.java.classLoader),
+        parcel.readValue(Double::class.java.classLoader) as? Double,
+        parcel.readValue(Double::class.java.classLoader) as? Double,
+        parcel.readValue(Double::class.java.classLoader) as? Double
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(target, flags)
+        parcel.writeValue(zoom)
+        parcel.writeValue(tilt)
+        parcel.writeValue(bearing)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CameraPosition> {
+        override fun createFromParcel(parcel: Parcel): CameraPosition {
+            return CameraPosition(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CameraPosition?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
 
 class CameraPositionState(var cameraPosition: CameraPosition = CameraPosition()) {

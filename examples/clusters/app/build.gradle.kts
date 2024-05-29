70c4e3d69f70fa0fa9ca48dfd02a37324c3df7d7
Jonas Vautherin <dev@jonas.vautherin.ch>
2024-05-29 16:39:55 +0200

    Update examples to use ramani-maplibre:0.4.0

--- examples/clusters/app/build.gradle.kts
+++ examples/clusters/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("org.ramani-maps:ramani-maplibre:0.3.0")
+    implementation("org.ramani-maps:ramani-maplibre:0.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

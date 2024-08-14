d0d918679dd1275b51a02592bb6bf48671e0483b
rinlv <rinlv.hn@gmail.com>
2024-08-14 17:55:59 +0700

    add example using MapEffect and DisposableMapEffect

--- examples/annotation-simple/app/src/main/java/org/ramani/example/annotation_simple/MainActivity.kt
+++ examples/annotation-simple/app/src/main/java/org/ramani/example/annotation_simple/MainActivity.kt
@@ -3,15 +3,26 @@ package org.ramani.example.annotation_simple
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.mapbox.android.gestures.StandardScaleGestureDetector
 import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.maps.MapLibreMap.OnScaleListener
 import org.ramani.compose.CameraPosition
 import org.ramani.compose.Circle
+import org.ramani.compose.DisposableMapEffect
+import org.ramani.compose.MapEffect
 import org.ramani.compose.MapLibre
 import org.ramani.compose.Polyline
 import org.ramani.example.annotation_simple.ui.theme.AnnotationSimpleTheme
@@ -32,23 +43,66 @@ class MainActivity : ComponentActivity() {
                 }
                 val circleCenter = rememberSaveable { mutableStateOf(LatLng(4.8, 46.0)) }
 
+                val scaleValue = rememberSaveable { mutableStateOf("") }
+
+                val scaleListener = object : OnScaleListener {
+                    override fun onScaleBegin(p0: StandardScaleGestureDetector) {
+                        scaleValue.value = "onScaleBegin"
+                    }
+
+                    override fun onScale(p0: StandardScaleGestureDetector) {
+                        scaleValue.value = "onScale"
+                    }
+
+                    override fun onScaleEnd(p0: StandardScaleGestureDetector) {
+                        scaleValue.value = "onScaleEnd"
+                    }
+
+                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MapLibre(
-                        modifier = Modifier.fillMaxSize(),
-                        styleUrl = resources.getString(R.string.maplibre_style_url),
-                        cameraPosition = cameraPosition.value,
-                    ) {
-                        Circle(
-                            center = circleCenter.value,
-                            radius = 50F,
-                            isDraggable = true,
-                            borderWidth = 2F,
-                            onCenterDragged = { center -> circleCenter.value = center }
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        MapLibre(
+                            modifier = Modifier.fillMaxSize(),
+                            styleUrl = resources.getString(R.string.maplibre_style_url),
+                            cameraPosition = cameraPosition.value,
+                        ) {
+                            Circle(
+                                center = circleCenter.value,
+                                radius = 50F,
+                                isDraggable = true,
+                                borderWidth = 2F,
+                                onCenterDragged = { center -> circleCenter.value = center }
+                            )
+                            Polyline(points = polylinePoints, color = "Red", lineWidth = 5.0F)
+
+                            MapEffect(key1 = Unit) {
+                                it.getMapAsync { mapLibre ->
+                                    mapLibre.setMaxZoomPreference(21.0)
+                                    mapLibre.setMinZoomPreference(5.0)
+                                }
+                            }
+
+                            DisposableMapEffect(key1 = Unit) {
+                                it.getMapAsync { mapLibre ->
+                                    mapLibre.addOnScaleListener(scaleListener)
+                                }
+                                onDispose {
+                                    it.getMapAsync { mapLibre ->
+                                        mapLibre.removeOnScaleListener(scaleListener)
+                                    }
+                                }
+                            }
+                        }
+
+                        Text(
+                            "Scale status = ${scaleValue.value}",
+                            style = TextStyle(color = Color.Black),
+                            modifier = Modifier.align(Alignment.TopStart).padding(8.dp)
                         )
-                        Polyline(points = polylinePoints, color = "Red", lineWidth = 5.0F)
                     }
                 }
             }
